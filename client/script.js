904df01bd63ab42959ff2bf38692efefbd3ccce4
Ali Ghezal <alighezal773@gmail.com>
2025-03-01 18:58:56 +0000

    Removed redundant code

--- client/script.js
+++ client/script.js
@@ -52,8 +52,8 @@ function recordTime(){
 
 function clearRace(){
     participant = 1
-    let timeRecorded = document.querySelectorAll(".bananas").forEach(element => element.remove());
-    let participants = document.querySelectorAll(".participant").forEach(element => element.remove())
+    document.querySelectorAll(".bananas").forEach(element => element.remove());
+    document.querySelectorAll(".participant").forEach(element => element.remove())
 }
 
 function recordTime() {
